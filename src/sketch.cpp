255209d6c4f4942eee2c7ee7c1850b9584168a07
Yohei Igata <yohei.igata@gmail.com>
2019-01-17 11:43:24 +0900

    Add STM32RTC

--- src/sketch.cpp
+++ src/sketch.cpp
@@ -1,50 +1,11 @@
-// GPIOの割り込み
-//
-// Grove-Buttonの2回路版。端的に言えばプルダウンのスイッチ2つ。
-//
-// VCC
-//  +                   D20
-//  |                    o
-//  |                    |
-//  |            _/      |     ___
-//  |----------o/  o-----o----|___|------
-//  |                          10k      |
-//  |                                   |
-//  |            _/            ___      |
-//  |----------o/  o-----o----|___|-----o
-//                       |     10k      |
-//                       |              |
-//                       o              |
-//                      D19            ===
-//                                     GND
-#include "main.h"
 #include <WioCellLibforArduino.h>
 
-WioCellular Wio;
-
-void push_d19()
-{
-  SerialUSB.println("Push D19");
-}
-
-void push_d20()
-{
-  SerialUSB.println("Push D20");
-}
-
 void setup() {
-  delay(200);
-  SerialUSB.begin(115200);
+  // put your setup code here, to run once:
 
-  Wio.Init();
-  Wio.PowerSupplyGrove(true);
-
-  pinMode(WIO_D19, INPUT);
-  attachInterrupt(WIO_D19, push_d19, FALLING);
-
-  pinMode(WIO_D20, INPUT);
-  attachInterrupt(WIO_D20, push_d20, FALLING);
 }
 
 void loop() {
+  // put your main code here, to run repeatedly:
+
 }
